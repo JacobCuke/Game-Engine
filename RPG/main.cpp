dbafc0b10d06107dbe57528992f564260b9a6bf8
Jacob Cuke <jacobcuke@gmail.com>
2020-02-14 18:22:48 +0900

    Initial Commit

---
+++ RPG/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RPG
+//
+//  Created by Jacob Cuke on 2020/02/14.
+//  Copyright © 2020 Jacob Cuke. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
